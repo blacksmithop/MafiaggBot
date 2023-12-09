3ab1a9b866b8b801cff9a74ccf1e4a88f52ae5ad
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2023-12-09 15:11:47 +0000

    feat: Rework logic to fetch decks

--- bot/deck.py
+++ bot/deck.py
@@ -1,59 +1,100 @@
 from requests import Session
-from json import loads, dump, load
-from os import getcwd, path, mkdir
+from json import dump, load
+from os import getcwd, path
 from bot.customtypes import DeckDict
+from pydantic import BaseModel, validator
+from datetime import datetime
+from typing import List
+from pathlib import Path
 
+class Pagination(BaseModel):
+    page: int
+    numPages: int
+    total: int
 
-class Deck:
+    
+class Character(BaseModel):
+    playedId: int
+    name: str
+    avatarUrl: str
+    backgroundColor: str
+    
+class Deck(BaseModel):
+    name: str
+    version: int
+    key: str
+    builtin: bool
+    deckSize: int
+    uploadTimestamp: int
+    sampleCharacters: List[Character]
+    
+    @validator("uploadTimestamp", pre=True)
+    def uploadTimestamp_validate(cls, uploadTimestamp):
+        return datetime.fromtimestamp(uploadTimestamp)
+
+class DeckData(BaseModel):
+    pagination: Pagination
+    decks: List[Deck]
+    
+
+class GenerateDeck:
     URL = "https://mafia.gg/api/decks"
+    DECK_DIR = "./data/decks/deck.json"
+    
+    def __init__(self, update: bool = False):
+        self.getDecks(update)
+            
+    def updateOrCreate(self, update: bool):
+        self.createDeckDir()
+        self.generateDeckData()
+    
+    def getDecks(self, update: bool):
+        if path.isfile(self.DECK_DIR) and not update:
+            print("Loading to memory (deck)")
+            with open(self.DECK_DIR) as f:
+                data = load(f)
+                dataset = DeckData(**data)
+                self.dataset = dataset.decks
+        else:
+            self.updateOrCreate()
 
-    def __init__(self, payload, update: bool = False):
-        self.json = None
-        self.decks = DeckDict()
-        self.loc = None
-        self.create_dir()
-        self.get_decks(payload, update)
-        self.store_deck(update)
-
-    def get_decks(self, payload, update):
-        loc = path.join(self.loc, 'deck.json')
-        if path.isfile(loc):
-            print("Deck file found")
-            if not update:
+    def generateDeckData(self):
+        # First we need to get the pagination data
+        with Session() as sess:
+            resp = sess.get(self.URL)
+            if resp.status_code != 200:
                 return
-        with Session() as s:
-            num = loads(s.get(self.URL, cookies=payload).content)
-        num = num['pagination']['numPages']
-        print(f"Found {num} pages of decks")
-        for i in range(1, num+1):
-            print(f"Getting deck data {i}/12")
-            with Session() as s:
-                self.json = loads(s.get(f'{self.URL}?filter&page={i}', cookies=payload).content)
+            
+            data = resp.json()
+            dataset = DeckData(**data)
+            
+        page = dataset.pagination.page
+        numPages= dataset.pagination.numPages
+        print(f"Page {page} of {numPages}")
+        
+        # Now iteratively append page data for decks 
+        for page in range(2, numPages+1):
+            with Session() as sess:
+                print(f"Page {page} of {numPages}")
+                data = sess.get(f'{self.URL}?page={page}').json()
+                _decks = DeckData(**data).decks
+                dataset.decks.extend(_decks)
 
-            for d in self.json['decks']:
-                self.decks[d['name'].lower()] = d['key']
+        dataset.pagination.page = page
+        self.store_deck()
+        self.dataset = dataset.decks
+        
+    def createDeckDir(self) -> None:
+        Path(self.DECK_DIR).mkdir(parents=True, exist_ok=True)
 
-    def create_dir(self) -> None:
-        self.loc = path.join(getcwd(), 'bot/data')
-        try:
-            mkdir(self.loc)
-        except OSError:
-            print("Data folder already exists")
+    def store_deck(self) -> None:
+        with open(self.DECK_DIR, 'w') as f:
+            dataset = dict(self.dataset)
+            dump(dataset, f)
+            print("Saved deck data to file")
 
-    def store_deck(self, update) -> None:
-        loc = path.join(self.loc, 'deck.json')
-        if path.isfile(loc):
-            if not update:
-                print("Loading to memory (deck)")
-                with open(loc) as f:
-                    self.decks = DeckDict(load(f))
-                return
-        with open(loc, 'w') as f:
-            dump(self.decks, f)
-            print("Wrote deck data to file")
-
-    def search_deck(self, name: str) -> [bool, str]:
-        return self.decks[name.lower()]
+    def search(self, name: str) -> [bool, str]:
+        return next(item for item in self.dataset if item.name.lower() == name)
 
 
 
