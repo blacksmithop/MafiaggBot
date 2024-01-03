237d36dc0b9197c83bd35f8fb2966e59e4e455e1
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2024-01-03 09:39:23 +0000

    feat: Move decorator

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,20 @@
 from difflib import SequenceMatcher
+from functools import wraps
+from typing import Dict
 
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
+
+
+def ignore_bot_message(func):
+    @wraps(func)
+    def wrapper(self, payload: Dict):
+        if payload["type"] == "chat":
+            if "from" in payload:
+                if payload["from"]["userId"] == self.id:
+                    return
+        res = func(self, payload)
+        return res
+
+    return wrapper
