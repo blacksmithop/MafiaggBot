0e1abc9c5c0b0d5fd31b4fbb0d9fb587605ff9cd
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-12-12 16:54:45 +0530

    feat: Move models

--- app/utils/player_stats.py
+++ app/utils/player_stats.py
@@ -1,30 +1,22 @@
-from pydantic import BaseModel
+from app.utils.models import PlayerStats
 from mafiagg.user import GetUser
 
 
-player = GetUser(cookie=None) # We dont need auth to fetch players
-
-class PlayerStats(BaseModel):
-    id: int
-    username: str
-    createdAt: str
-    totalGames: int = 1
-    winRate: int = 0
-    townWinRate: int = 0
-    mafiaWinRate: int = 0
-    neutralWinRate: int = 0
+player = GetUser(cookie=None)  # We dont need auth to fetch players
 
 # TODO: Implement search by name
-# First we need to index all players
 
-def get_player_data(id: int=None, username: str= None, report=False):
+
+def get_player_data(id: int = None, username: str = None, report=False):
     if id:
         player = GetUser().get_user(id=id)
         return player
     if username:
-        player = GetUser().get_user(id=533610) # TODO: Look up DB for game reports
+        player = GetUser().get_user(id=533610)  # TODO: Look up DB for game reports
         if report:
-            player = PlayerStats(id=player.id, username=player.username, createdAt=player.createdAt)
+            player = PlayerStats(
+                id=player.id, username=player.username, createdAt=player.createdAt
+            )
             return player
         # TODO: If not exact match, get all similar player names
         return [player]
