8b12750b5778b99718e393ef1f1e40f017c20e48
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-12-12 14:19:16 +0530

    chore: Return empty list if connection failed TODO: Raise error and log

--- app/router/dashboard.py
+++ app/router/dashboard.py
@@ -9,7 +9,10 @@ router = APIRouter()
 
 @router.get("/get_rooms")
 async def get_rooms(request: Request) -> List[Room]:
-    cookie = auth.get_cookie_data()
-    room = GetRoom(cookie=cookie)
-    rooms = room.get_rooms()
-    return rooms
+    try:
+        cookie = auth.get_cookie_data()
+        room = GetRoom(cookie=cookie)
+        rooms = room.get_rooms()
+        return rooms
+    except:
+        return []
