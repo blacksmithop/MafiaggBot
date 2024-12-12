4309a58f02c6828a95f93d5313c1ca334d64ce69
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-12-12 17:13:56 +0530

    feat: Return UUID token

--- app/router/auth.py
+++ app/router/auth.py
@@ -4,7 +4,7 @@ from mafiagg.credential_manager import CredentialManager
 from app.utils.database import UserCollection
 from app.utils.models import Login
 from mafiagg.models.models import SearchUser
-
+from uuid import uuid4
 
 router = APIRouter()
 
@@ -15,12 +15,14 @@ user_db = UserCollection()
 # TODO: Implement dependency
 @router.post("/login")
 async def get_rooms(request: Request, payload: Login) -> Dict:
-    cred = CredentialManager(username=payload.username, password=payload.password)
-    cookie = cred.get_cookie_data()
+    login_token = uuid4().hex
+    return login_token
+    # cred = CredentialManager(username=payload.username, password=payload.password)
+    # cookie = cred.get_cookie_data()
 
-    user: SearchUser = user_db.get_user(cookie=cookie)
-    user_id = user.id
-    if user_db.check_if_user_exists(user_id=user_id):
-        return cookie
-    user_db.add_user(user_id=user.id, user_name=user.username)
-    return cookie
+    # user: SearchUser = user_db.get_user(cookie=cookie)
+    # user_id = user.id
+    # if user_db.check_if_user_exists(user_id=user_id):
+    #     return cookie
+    # user_db.add_user(user_id=user.id, user_name=user.username)
+    # return cookie
